ea9d44ccd97d5323c9e90830c2d8fca720fbab6c
cyriltasse <cyril_tasse@yahoo.fr>
2014-11-22 11:26:43 +0100

    minor

--- CohJones/CHMOD.sh
+++ CohJones/CHMOD.sh
@@ -8,7 +8,7 @@
 
 #rm *.pyx
 
-./killMS.py -h
+./CohJones.py -h
 ./MakeModel.py -h
 python -c "import dsreg"
 python -c "import dsc"
@@ -16,6 +16,6 @@ python -c "import dsc"
 
 chmod 611 *.py
 chmod 655 *.pyc
-chmod 755 killMS.py
+chmod 755 CohJones.py
 chmod 755 MakeModel.py
 chmod 755 dsreg.py
