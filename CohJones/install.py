1f18354a23d8c726017145e35b5b1ce147afa3a2
cyriltasse <cyril_tasse@yahoo.fr>
2014-12-04 11:16:04 +0100

    inor

--- CohJones/install.py
+++ CohJones/install.py
@@ -4,7 +4,7 @@ def install():
     os.system("python CompilCython.py build_ext --inplace")
     os.system("mkdir bin")
     os.system("cp *.so bin/.")
-    os.system("cp CHMOD.sh ClassMS.py ClassPredict.py  killMS.py  MakeModel.py dsreg.py bin/.")
+    os.system("cp CHMOD.sh ClassMS.py ClassPredict.py  CohJones.py  MakeModel.py dsreg.py bin/.")
 
 if __name__=="__main__":
     install()
